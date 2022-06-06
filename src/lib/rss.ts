022f14ceabdc9678226e3ebb81247b332969cd39
Jake He <zhex900@users.noreply.github.com>
2022-06-06 23:34:13 +1000

    remove husky

--- src/lib/rss.ts
+++ src/lib/rss.ts
@@ -1,29 +1,29 @@
-import format from 'date-fns/format';
+// import format from 'date-fns/format';
 import fs from 'fs';
 
-import { getAllFilesFrontmatter } from '@/lib/mdx';
+// import { getAllFilesFrontmatter } from '@/lib/mdx';
 
 export async function getRssXml() {
-  const frontmatters = await getAllFilesFrontmatter('blog');
+  // const frontmatters = await getAllFilesFrontmatter('blog');
 
   const blogUrl = 'https://theodorusclarence.com/blog';
 
-  const itemXml = frontmatters
-    .filter((fm) => !fm.slug.startsWith('id-'))
-    .map(({ slug, title, description, publishedAt, lastUpdated }) =>
-      `
-    <item>
-      <title>${cdata(title)}</title>
-      <description>${cdata(description)}</description>
-      <link>${blogUrl}/${slug}</link>
-      <guid>${blogUrl}/${slug}</guid>
-      <pubDate>${format(
-        new Date(lastUpdated ?? publishedAt),
-        'yyyy-MM-dd'
-      )}</pubDate>
-    </item>
-    `.trim()
-    );
+  // const itemXml = frontmatters
+  //   .filter((fm) => !fm.slug.startsWith('id-'))
+  //   .map(({ slug, title, description, publishedAt, lastUpdated }) =>
+  //     `
+  //   <item>
+  //     <title>${cdata(title)}</title>
+  //     <description>${cdata(description)}</description>
+  //     <link>${blogUrl}/${slug}</link>
+  //     <guid>${blogUrl}/${slug}</guid>
+  //     <pubDate>${format(
+  //       new Date(lastUpdated ?? publishedAt),
+  //       'yyyy-MM-dd'
+  //     )}</pubDate>
+  //   </item>
+  //   `.trim()
+  //   );
 
   return `
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:blogChannel="${blogUrl}">
@@ -33,15 +33,14 @@ export async function getRssXml() {
         <description>The Theodorus Clarence Blog, thoughts, mental models, and tutorials about front-end development.</description>
         <language>en</language>
         <ttl>40</ttl>
-        ${itemXml.join('\n')}
       </channel>
     </rss>
   `.trim();
 }
 
-function cdata(s: string) {
-  return `<![CDATA[${s}]]>`;
-}
+// function cdata(s: string) {
+//   return `<![CDATA[${s}]]>`;
+// }
 
 export async function generateRss() {
   const xml = await getRssXml();
