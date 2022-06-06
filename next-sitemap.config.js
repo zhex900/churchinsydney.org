838505f83a34f8be1475ef27d573d4fea31574fd
Jake He <zhex900@users.noreply.github.com>
2022-06-06 23:46:05 +1000

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://theodorusclarence.com/',
+  siteUrl: 'https://churchinsydney.org/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
