e599a19f132058262aec6b7bcd90e999abda8757
Jake He <zhex900@users.noreply.github.com>
2022-06-17 22:56:18 +1000

    remove pwa (#18)

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-
-module.exports = withPWA({
+module.exports = {
   images: {
     domains: ['media.churchinsydney.org'],
-  },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
   },
   i18n: {
     locales: ['en', 'zh-CN'], // Array with the languages that you want to use
@@ -21,4 +11,4 @@ module.exports = withPWA({
     localeDetection: true,
   },
   trailingSlash: true,
-});
+};
