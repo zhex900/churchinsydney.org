32bbb41174dd31e1d749614c58078f3f7b9ea37c
GitHubEason <31057002+GitHubEason@users.noreply.github.com>
2022-08-02 22:23:54 +1000

    test: 71-e2e-our-belief (#76)
    * test: 71-e2e-our-belief
    
    * feat: add test for mobile
    
    * e2e
    
    * clean up
    
    Co-authored-by: Jake He <zhex900@users.noreply.github.com>
    Co-authored-by: Jake He <zhex900@gmail.com>

---
+++ cypress/e2e/ourBelief.cy.ts
@@ -0,0 +1,16 @@
+type ViewportPreset = 'macbook-13' | 'iphone-x';
+
+describe('Our Belief page', () => {
+  ['macbook-13', 'iphone-x'].forEach((portView) => {
+    it(`[${portView}] Our belief page should open after click on the our-belief menu`, () => {
+      cy.viewport(portView as ViewportPreset);
+      cy.visit('/');
+      cy.title().should('eq', 'Church in Sydney');
+      cy.get('[href="/our-belief/"]').click();
+      cy.title().should('eq', 'Our Belief | churchinsydney.org');
+      cy.url().should('include', '/our-belief/');
+    });
+  });
+});
+
+export {};
