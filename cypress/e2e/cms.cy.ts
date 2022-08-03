d10a77c07b009510b853a88158b206bc73434ee5
Jake He <zhex900@gmail.com>
2022-08-03 18:04:50 +1000

    cms ci

--- cypress/e2e/cms.cy.ts
+++ cypress/e2e/cms.cy.ts
@@ -1,7 +1,9 @@
 describe('CMS', () => {
   it('cms subdomain should redirect and login into cms should work', () => {
     cy.visit('https://cms.churchinsydney.org');
-
+    cy.intercept('**/settings').as('settings');
+    cy.intercept('**/auth').as('auth');
+    cy.intercept('**/users/me/track/page').as('page');
     cy.url().then((url) => {
       cy.request('POST', `${url.replace('/admin', '')}auth/login`, {
         email: Cypress.env('CMS_CYPRESS_USERNAME'),
@@ -10,6 +12,13 @@ describe('CMS', () => {
       });
       cy.reload();
       cy.contains('Cypress is currently authenticated.').should('be.visible');
+      cy.contains('Continue').click();
+      cy.wait('@settings');
+      cy.wait('@auth');
+      cy.wait('@page');
+      cy.get('.name').should('contain.text', 'churchinsydney.org');
+      cy.contains('No Collections').should('be.visible');
+      cy.request('POST', `${url.replace('/admin', '')}auth/logout`);
     });
   });
 });
